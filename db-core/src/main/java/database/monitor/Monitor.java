4452100b9596b37ea5ea84e7b650f9191820300e
Boris <svetlovvv9@gmail.com>
2024-06-21 00:08:18 +0500

    Demo commit

--- db-core/src/main/java/database/monitor/Monitor.java
+++ db-core/src/main/java/database/monitor/Monitor.java
@@ -1,8 +1,9 @@
 package database.monitor;
+
 import database.api.CommandHistory;
+import database.api.DatabaseAPI;
 import database.api.DatabaseEditor;
 import database.api.utils.OUTPUT_TYPE;
-import database.api.DatabaseAPI;
 import database.service.tools.SQLListener;
 import database.service.tools.grammar.SQLGrammarLexer;
 import database.service.tools.grammar.SQLGrammarParser;
@@ -17,15 +18,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Optional;
+import java.util.Scanner;
 
 public class Monitor {
-    private static final java.util.Scanner in = new java.util.Scanner(System.in);
-    private static final Logger logger = LogManager.getLogger(Monitor.class);
-    /**
-     * Reads SQL commands from a file and parses them.
-     *
-     * @param fileName the name of the file containing SQL commands.
-     */
+    private static final Scanner in = new Scanner(System.in);
+    private static final Logger logger = LogManager.getLogger(MonitorOld.class);
+
     public static void readCommandsFromFile(String fileName, DatabaseAPI databaseAPI, OUTPUT_TYPE outputType, Optional<String> filePath) {
         try(BufferedReader reader = new BufferedReader(new FileReader(fileName))){
             StringBuilder commands = new StringBuilder();
@@ -50,11 +48,20 @@ public class Monitor {
         }
     }
 
-    /**
-     * Reads SQL commands from the command line until the user enters ":q".
-     * Parses the accumulated commands.
-     */
     public static void readCommandsFromCommandLine(DatabaseAPI databaseAPI, OUTPUT_TYPE outputType, Optional<String> filePath) {
+        System.out.println("Choose output mode: Console (C) or File (F)");
+        String outputMode = in.next();
+        if (outputMode.equalsIgnoreCase("C")) {
+            System.out.println("Outputting to console:");
+            readCommandsFromCommandLine(databaseAPI, outputType, filePath);
+        } else if (outputMode.equalsIgnoreCase("F")) {
+            System.out.println("Enter the file name:");
+            String fileName = in.next();
+            readCommandsFromCommandLine(databaseAPI, outputType, filePath);
+        } else {
+            System.out.println("Invalid choice.");
+        }
+
         StringBuilder commands = new StringBuilder();
         String line = in.next();
         while(!line.equals(":q")){
@@ -77,58 +84,58 @@ public class Monitor {
         }
     }
 
-//    public static void main(String[] args) {
-//        DatabaseAPI databaseAPI;
-//        databaseAPI = new DatabaseAPI();
-//        databaseAPI.setActiveEditor(new DatabaseEditor());
-//        databaseAPI.setHistory(new CommandHistory());
-//        final java.util.Scanner scanner = new java.util.Scanner(System.in);
-//        Config config = new Config();
-//        System.out.println("Greetings traveler!");
-//        System.out.println("Choose the working mode: Command Line Interface (CLI) or File System (FS), or type HELP to check our cool commands");
-//        boolean flag = true;
-//        boolean mainFlag = true;
-//        while (mainFlag) {
-//            String input = scanner.nextLine();
-//            if (input.equalsIgnoreCase("CLI")) {
-//                System.out.println("You chose Command Line Interface.");
-//                while (flag) {
-//                    System.out.println("Enter a command, or type ':q' to quit:");
-//                    readCommandsFromCommandLine(databaseAPI);
-//                    break;
-//                }
-//                break;
-//            } else if (input.equalsIgnoreCase("FS")) {
-//                System.out.println("You chose File System.");
-//                System.out.println("Enter the file name:");
-//                String fileName = in.next();
-//                try {
-//                    readCommandsFromFile(fileName, databaseAPI);
-//                } catch (IOException e) {
-//                    throw new RuntimeException(e);
-//                }
-//                break;
-//            } else if (input.equalsIgnoreCase("HELP")) {
-//                for (String command  {
-//                    System.out.println(command);
-//                }
-//                System.out.println("Choose the working mode: Command Line Interface (CLI) or File System (FS), or type HELP to check our cool commands");
-//            } else {
-//                System.out.println("Invalid choice.");
-//            }
-//        }
-//
-//    }
-//
-//    /*private static void runCommandInThread(String command) {
-//        Thread commandThread = new Thread(() -> {
-//            System.out.println("Running command: " + command);
-//        });
-//        commandThread.start();
-//        try {
-//            commandThread.join();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }*/
+    public static void handleCommandLine(DatabaseAPI databaseAPI) {
+        System.out.println("You chose Command Line Interface.");
+        System.out.println("Enter a command, or type ':q' to quit:");
+        readCommandsFromCommandLine(databaseAPI);
+    }
+
+    public static void handleFileSystem(DatabaseAPI databaseAPI) {
+        System.out.println("You chose File System.");
+        System.out.println("Choose output mode: Console (C) or File (F)");
+        String outputMode = in.next();
+
+        if (outputMode.equalsIgnoreCase("C")) {
+            System.out.println("Outputting to console:");
+            readCommandsFromFile(databaseAPI);
+        } else if (outputMode.equalsIgnoreCase("F")) {
+            System.out.println("Enter the file name:");
+            String fileName = in.next();
+            try {
+                readCommandsFromFile(fileName, databaseAPI);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            System.out.println("Invalid choice.");
+        }
+    }
+
+    public static void displayHelpCommands(Config config) {
+        System.out.println("Choose the working mode: Command Line Interface (CLI) or File System (FS), or type HELP to check our cool commands");
+    }
+
+    public static void main(String[] args) {
+        DatabaseAPI databaseAPI = new DatabaseAPI();
+        databaseAPI.setActiveEditor(new DatabaseEditor());
+        databaseAPI.setHistory(new CommandHistory());
+        Config config = new Config();
+        System.out.println("Greetings traveler!");
+        System.out.println("Choose the working mode: Command Line Interface (CLI) or File System (FS), or type HELP to check our cool commands");
+        boolean mainFlag = true;
+        while (mainFlag) {
+            String input = in.nextLine();
+            if (input.equalsIgnoreCase("CLI")) {
+                handleCommandLine(databaseAPI);
+                break;
+            } else if (input.equalsIgnoreCase("FS")) {
+                handleFileSystem(databaseAPI);
+                break;
+            } else if (input.equalsIgnoreCase("HELP")) {
+                displayHelpCommands(config);
+            } else {
+                System.out.println("Invalid choice.");
+            }
+        }
+    }
 }
